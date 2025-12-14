5325b03d276a87f434e382c6df3878384be3d60b
Abhijeet Raj <vermavijit16144@gmail.com>
2025-12-15 01:20:33 +0530

    first commit

---
+++ src/routes/bag.jsx
@@ -0,0 +1,25 @@
+import BagItem from "../components/BagItem";
+import BagSummary from "../components/BagSummary";
+import { useSelector } from "react-redux";
+
+const Bag = () => {
+  const bagItems = useSelector((state) => state.bag);
+  const items = useSelector((state) => state.items);
+  const finalItems = items.filter((item) => bagItems.includes(item.id));
+
+  return (
+    <main>
+      <div className="bag-page">
+        <div className="bag-items-container">
+          {finalItems.map((item) => (
+            <BagItem item={item} />
+          ))}
+        </div>
+
+        <BagSummary></BagSummary>
+      </div>
+    </main>
+  );
+};
+
+export default Bag;
